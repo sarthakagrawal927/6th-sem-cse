a3c3a644dc5d353518f85d95081d888ba319246a
sarthakagrawal927 <sarthakagrawal927@gmail.com>
2021-03-17 09:06:06 +0530

    IT lab4

---
+++ IT_LAB/lab4/q2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findPair(self,l, target):
+        lookup = {}
+        for i  in range(len(l)):
+            if target - l[i] in lookup:
+                return {lookup[target-l[i]]+1, i+1}
+            lookup[l[i]] = i           
+
+print(Solution().findPair([4,3,5,8,7,9,1],6))
