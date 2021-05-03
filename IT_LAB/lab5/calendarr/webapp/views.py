cb40a3cb81bc272de48e6b91d25f84e9a78ee91d
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-05-03 10:04:09 +0530

    more stolen code added

---
+++ IT_LAB/lab5/calendarr/webapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
+
+def index(request,year,month):
+    year = int(year)
+    month = int(month)
+    if year < 1900 or year > 2099:
+         year = date.today().year
+    month_name = calendar.month_name[month]
+    title = "MyClub Event Calendar - %s %s" % (month_name,year)
+    cal = HTMLCalendar().formatmonth(year, month)
+    return render(request,'base.html',{'title':title,'cal':cal})
+
+
+def calculate(request):
+    return render(request,'cal.html')
+
+def marks(request):
+    return render(request,'marks.html')    
+
+def emp(request):
+    return render(request,'employee.html')    
+
+def mag(request):
+    return render(request,'magazine.html')    
+
+
+def cap(request):
+    return render(request,'captcha.html')
