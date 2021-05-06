cc1dddd04fb283bd3a95296f77f740b09cae38e5
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-05-06 12:59:35 +0530

    removed the advanced shit

--- IT_LAB/lab5/tooAdvanved/3/main/calculator/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, DateInput
-from .models import Calculator
-
-
-class CalculatorForm(ModelForm):
-    class Meta:
-        model = Calculator
-        fields = '__all__'
