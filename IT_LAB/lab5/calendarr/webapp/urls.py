124de48df5046454e1a1e36ac66d9715533829eb
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-05-06 14:11:54 +0530

    added additional 2nd

--- IT_LAB/lab5/calendarr/webapp/urls.py
+++ IT_LAB/lab5/calendarr/webapp/urls.py
@@ -1,15 +1,14 @@
 from . import views
-from  django.urls import path,re_path
+from django.urls import path, re_path
 
 urlpatterns = [
-    path('calculate/',views.calculate,name='calculate'),
-    path('marks/',views.marks,name='marks'),
-    path('emp/',views.emp,name='emp'),
-    path('mag/',views.mag,name='mag'),
-    path('cap/',views.cap,name='cap'),
-    path('',views.index,name='index'),
-    re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/', views.index, name='index')
+    path('calculate/', views.calculate, name='calculate'),
+    path('marks/', views.marks, name='marks'),
+    path('emp/', views.emp, name='emp'),
+    path('mag/', views.mag, name='mag'),
+    path('cap/', views.cap, name='cap'),
+    path('metadata/', views.book, name='book'),
+    path('', views.index, name='index'),
+    re_path(
+        r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/', views.index, name='index')
 ]
-   
-
-
--- IT_LAB/lab5/calendarr/webapp/views.py
+++ IT_LAB/lab5/calendarr/webapp/views.py
@@ -3,29 +3,37 @@ from datetime import date
 import calendar
 from calendar import HTMLCalendar
 
-def index(request,year,month):
+
+def index(request, year, month):
     year = int(year)
     month = int(month)
     if year < 1900 or year > 2099:
-         year = date.today().year
+        year = date.today().year
     month_name = calendar.month_name[month]
-    title = "MyClub Event Calendar - %s %s" % (month_name,year)
+    title = "MyClub Event Calendar - %s %s" % (month_name, year)
     cal = HTMLCalendar().formatmonth(year, month)
-    return render(request,'base.html',{'title':title,'cal':cal})
+    return render(request, 'base.html', {'title': title, 'cal': cal})
 
 
 def calculate(request):
-    return render(request,'cal.html')
+    return render(request, 'cal.html')
+
 
 def marks(request):
-    return render(request,'marks.html')    
+    return render(request, 'marks.html')
+
 
 def emp(request):
-    return render(request,'employee.html')    
+    return render(request, 'employee.html')
+
 
 def mag(request):
-    return render(request,'magazine.html')    
+    return render(request, 'magazine.html')
 
 
 def cap(request):
-    return render(request,'captcha.html')
+    return render(request, 'captcha.html')
+
+
+def book(request):
+    return render(request, 'book.html')
